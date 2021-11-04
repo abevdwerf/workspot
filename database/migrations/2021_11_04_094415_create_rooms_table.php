e2bc742f81da3e277b4cf1196c3751d4362ddd44
Cas Goorman <casgo98@hotmail.nl>
2021-11-04 13:40:56 +0100

    first

---
+++ database/migrations/2021_11_04_094415_create_rooms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('location_id')->constrained();
+            $table->string('type');
+            $table->integer('seats_total');
+            $table->integer('seats_available');
+            $table->string('highlighted_map');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
