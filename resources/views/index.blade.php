0596148cdc772db9f2825ccd237b3d0ded5ef709
Cas Goorman <casgo98@hotmail.nl>
2021-11-05 12:53:26 +0100

    added csrf token and method to form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,7 +23,9 @@
     <main>
         <div class="container">
             <section class="finder">
-                <form class="form__group" action="{{ url("./rooms") }}" method="post">
+                <form class="form__group" action="{{ url("/rooms") }}" method="POST">
+                    @csrf
+                    @method('get')
                     <div class="form__input-wrapper">
                         <label class="form__label flex" for="location-input">
                             <svg xmlns="http://www.w3.org/2000/svg" width="11.667" height="16.667" viewBox="0 0 11.667 16.667"><path d="M10.833,2A5.829,5.829,0,0,0,5,7.833c0,4.375,5.833,10.833,5.833,10.833s5.833-6.458,5.833-10.833A5.829,5.829,0,0,0,10.833,2Zm0,7.917a2.083,2.083,0,1,1,2.083-2.083A2.084,2.084,0,0,1,10.833,9.917Z" transform="translate(-5 -2)" fill="#b1b1b1"/></svg>
