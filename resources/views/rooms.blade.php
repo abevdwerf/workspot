12bec4c6f7105d8a26277bf9449ccda0697f653a
Rubenvandeven2001 <484553@student.fontys.nl>
2021-11-17 12:34:16 +0100

    Room labels on mobile

--- resources/views/rooms.blade.php
+++ resources/views/rooms.blade.php
@@ -38,7 +38,7 @@
                     Go back
                 </a>
 
-                <div style="position: relative;">
+                <!-- <div style="position: relative;">
                     <a href="#" class="breadcrumb__spots-btn flex" onclick="sortToggle()">
                         <span>Spots available</span>
 
@@ -55,7 +55,7 @@
                         <a href="#" class="form__dropdown-option">4 spots</a>
                         <a href="#" class="form__dropdown-option">5 spots</a>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
 
@@ -69,13 +69,27 @@
                     @foreach($rooms as $room)
                         <a href="{{ $room->highlighted_map ? url('workspace/'.$room->id ) : '#' }}" class="room">
                             <div class="room__content flex">
-                                <h3 class="h3">{{$room->name}}</h3>
-                                <span class="room__floor">{{$room->getFloorName()}}</span>
+                                <h3 class="h3">{{ $room->name }}</h3>
+                                <span class="room__floor">{{ $room->getFloorName() }}</span>
                             </div>
 
                             <span class="room__spots flex">
-                                <span class="room__highlight">{{$room->seats_available}} / {{$room->seats_total}}</span> Spots
+                                <span class="room__highlight">{{ $room->seats_available }} / {{ $room->seats_total }}</span> Spots
                             </span>
+
+                            <div class="room__labels flex">
+                                @if ($room->type === "meeting room")
+                                    <div class="room__label flex room__label--meeting">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14.183" height="17" viewBox="0 0 14.183 17"><g transform="translate(0 0)"><path d="M91.126,8.189a3.962,3.962,0,0,0,2.9-1.2,3.962,3.962,0,0,0,1.2-2.9,3.962,3.962,0,0,0-1.2-2.9,4.094,4.094,0,0,0-5.79,0,3.962,3.962,0,0,0-1.2,2.895,3.962,3.962,0,0,0,1.2,2.9A3.963,3.963,0,0,0,91.126,8.189Zm0,0" transform="translate(-84.142)" /><path d="M14.149,252.177a10.114,10.114,0,0,0-.138-1.074,8.464,8.464,0,0,0-.264-1.08,5.335,5.335,0,0,0-.444-1.007,3.8,3.8,0,0,0-.67-.872,2.952,2.952,0,0,0-.962-.6,3.324,3.324,0,0,0-1.228-.222,1.246,1.246,0,0,0-.665.282c-.2.13-.433.281-.693.447a3.971,3.971,0,0,1-.9.4,3.483,3.483,0,0,1-2.194,0,3.962,3.962,0,0,1-.9-.4c-.258-.165-.491-.315-.694-.447a1.245,1.245,0,0,0-.665-.282,3.319,3.319,0,0,0-1.228.222,2.949,2.949,0,0,0-.962.6,3.8,3.8,0,0,0-.669.872,5.345,5.345,0,0,0-.444,1.007,8.484,8.484,0,0,0-.264,1.08,10.078,10.078,0,0,0-.138,1.075c-.023.325-.034.663-.034,1a2.823,2.823,0,0,0,.838,2.136A3.018,3.018,0,0,0,3,256.1h8.186a3.017,3.017,0,0,0,2.16-.788,2.821,2.821,0,0,0,.839-2.136c0-.343-.012-.68-.034-1Zm0,0" transform="translate(0 -239.104)" /></g></svg>
+                                        <span class="room__label-text">Meeting Room</span>
+                                    </div>
+                                @elseif ($room->type === "silent room")
+                                    <div class="room__label flex room__label--silent">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 17h-5v-10h5v10zm2-10v10l9 5v-20l-9 5zm15.324 4.993l1.646-1.659-1.324-1.324-1.651 1.67-1.665-1.648-1.316 1.318 1.67 1.657-1.65 1.669 1.318 1.317 1.658-1.672 1.666 1.653 1.324-1.325-1.676-1.656z"/></svg>
+                                        <span class="room__label-text">Silent Room</span>
+                                    </div>
+                                @endif
+                            </div>
                         </a>
                     @endforeach
                 @endif
@@ -83,9 +97,7 @@
         </div>
     </main>
 
-    <!-- <script src="{{asset('js/app.js')}}" type="text/javascript"></script> -->
-
-    <script type="text/javascript">
+    <!-- <script type="text/javascript">
         let dropdown = document.getElementById("sort-dropdown");
 
         dropdown.childNodes.forEach(dropdownOption => {
@@ -98,6 +110,6 @@
             if (dropdown.classList.contains("form__dropdown--active")) dropdown.classList.remove("form__dropdown--active");
             else dropdown.classList.add("form__dropdown--active");
         };
-    </script>
+    </script> -->
 </body>
 </html>
